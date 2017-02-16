61bd1c77c16aad6f24bf1286ad31da820433f391
Scott Matsumura <clepescott@gmail.com>
2017-02-15 21:37:16 -0800

    Added socket.io server for Android chat.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require('express')
+var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var io = require('socket.io');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,68 @@
+// Socket.io server for Socket.io app
 var express = require('express');
 var router = express.Router();
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+//var port = process.env.PORT || 3030;
+//
+//server.listen(port, function() {
+//  console.log('Server listening at port %d', port);
+//});
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Chat room
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // When the client emits 'new message', this listens and executes
+  socket.on('new message', function(data) {
+    // We tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function(username) {
+    if (addedUser) return;
+
+    // We store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // Echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // When the client emits 'typing', we broadcast it to others
+  socket.on('typing', function() {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // When the user disconnects, perform this
+  socket.on('disconnect', function() {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
 
 module.exports = router;
