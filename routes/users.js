91b7066adad5a73ee3dc3c1b5512f66a282d3018
Scott Matsumura <clepescott@gmail.com>
2017-02-20 03:35:55 -0800

    Add code for chat room.

--- routes/users.js
+++ routes/users.js
@@ -1,71 +1,9 @@
-// Socket.io server for Socket.io app
 var express = require('express');
 var router = express.Router();
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 80;
 
-server.listen(port, function() {
-  console.log('Server listening at port %d', port);
-});
-
-// Routing
-app.use(express.static(__dirname + '/public'));
-
-// Chat room
-
-var numUsers = 0;
-
-io.on('connection', function (socket) {
-  var addedUser = false;
-
-  // When the client emits 'new message', this listens and executes
-  socket.on('new message', function(data) {
-    // We tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function(username) {
-    if (addedUser) return;
-
-    // We store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // Echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // When the client emits 'typing', we broadcast it to others
-  socket.on('typing', function() {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // When the user disconnects, perform this
-  socket.on('disconnect', function() {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
+/* GET users page. */
+router.get('/users', function(req, res, next) {
+  res.sendFile(__dirname + '/users.html');
 });
 
 module.exports = router;
