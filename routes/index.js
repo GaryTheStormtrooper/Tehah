9c687ddcdb7454e1e9d3901d201f0768b7a3a28a
Scott Matsumura <clepescott@gmail.com>
2017-02-20 15:46:25 -0800

    More experimenting.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.htmlz');
 });
 
 module.exports = router;
