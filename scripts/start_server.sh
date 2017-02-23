d8edbbf8124c3a6f7f394b0f1f6219f21c9c9f93
Scott Matsumura <clepescott@gmail.com>
2017-02-23 03:25:31 -0800

    Minor changes.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 #service httpd start
 
-npm start > /dev/null 2> /dev/null < /dev/null & echo $! > node.pid
-echo $! > node.pid
+sudo npm start > /dev/null 2> /dev/null < /dev/null & echo $! > node.pid
+sudo echo $! > node.pid
